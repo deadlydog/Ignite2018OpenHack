a0272a6b1fa10a1ec4ea22ba884b34fdbaad5ed8
Daniel Schroeder <deadlydog@hotmail.com>
2018-09-27 09:47:58 -0600

    Group classes by domain

--- src/GetProductFunction/Products/GetProduct.cs
+++ src/GetProductFunction/Products/GetProduct.cs
@@ -6,7 +6,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 
-namespace Functions
+namespace Functions.Products
 {
     public static class GetProduct
     {
