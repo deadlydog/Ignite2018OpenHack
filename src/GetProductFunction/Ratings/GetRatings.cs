10116ba2d55b0c3e9feff7e4a0063804837307bb
Ricardo Perez <rperezs@gmail.com>
2018-09-27 12:51:00 -0600

    Get Ratings function

---
+++ src/GetProductFunction/Ratings/GetRatings.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Functions.Ratings
+{
+    public static class GetRatings
+    {
+        [FunctionName("GetRatings")]
+        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ratings")]HttpRequestMessage req, 
+            [CosmosDB(
+                databaseName: "FruitIcecream",
+                collectionName: "Ratings",
+                ConnectionStringSetting = "https://bfyoc-t15team.documents.azure.com:443/;AccountKey=pqxzZrcr8Ooo6zOg2qcgsesa77Ql2Ds6BmRwuZFMwSYOWtMFjA7N8e5wmxwfOFmHqM1JuJRotdiyLunM4ypbRQ==;")] IEnumerable<Rating> ratingItems, ILogger log)
+        {
+            log.Log(LogLevel.Information,"C# HTTP trigger function processed a request.");
+
+            return req.CreateResponse(HttpStatusCode.OK, ratingItems);
+        }
+    }
+}
